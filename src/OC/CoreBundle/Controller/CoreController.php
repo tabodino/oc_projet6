ee3489856da8b1540fe96dce1d8f786e396fd4e5
tabodino <tabodino@msn.com>
2016-07-24 12:02:46 +0200

    Iniatialisation projet 6

---
+++ src/OC/CoreBundle/Controller/CoreController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace OC\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('OCCoreBundle:Core:index.html.twig');
+    }
+}
